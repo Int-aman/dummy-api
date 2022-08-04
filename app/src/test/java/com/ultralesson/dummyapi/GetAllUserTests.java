d2bc40bcc149b067107e934985e12e5c8f13c077
Int-aman <aman.yadav3@swiggy.in>
2022-08-05 02:29:26 +0530

    Added test to get all users

---
+++ app/src/test/java/com/ultralesson/dummyapi/GetAllUserTests.java
@@ -0,0 +1,19 @@
+package com.ultralesson.dummyapi;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GetAllUserTests {
+
+    @Test
+    public void shouldGetAllUsers(){
+        given()
+                .header("app-id","62ec2b3c9e703f23bf6fc8a3")
+                .when()
+                    .get("https://dummyapi.io/data/v1/user")
+                .then()
+                    .statusCode(200)
+                    .log().body();
+    }
+}
