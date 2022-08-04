a6c3362be81e0290b08336597ebc95aec50da930
Int-aman <aman.yadav3@swiggy.in>
2022-08-05 02:41:34 +0530

    Added test to Create user

--- app/src/test/java/com/ultralesson/dummyapi/CreateUserTests.java
+++ app/src/test/java/com/ultralesson/dummyapi/CreateUserTests.java
@@ -1,4 +1,28 @@
 package com.ultralesson.dummyapi;
 
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class CreateUserTests {
+
+    @Test
+    public void shouldCreateUser(){
+
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("app-id","62ec2b3c9e703f23bf6fc8a3")
+                .body("{\n" +
+                        "    \"firstName\" : \"Ram\",\n" +
+                        "    \"lastName\" : \"Lal\",\n" +
+                        "    \"email\" : \"ramsssdwal@gmail.com\"\n" +
+                        "}")
+                .when()
+                    .post("https://dummyapi.io/data/v1/user/create")
+                .then()
+                    .log().body()
+                    .statusCode(200);
+    }
 }
